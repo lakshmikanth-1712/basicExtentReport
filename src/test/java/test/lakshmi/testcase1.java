75c3208d134cf9a6b903644babd7330758a82f09
lakshmikanth-1712 <63140402+lakshmikanth-1712@users.noreply.github.com>
2020-11-25 19:36:36 +0530

    Update testcase1.java

--- src/test/java/test/lakshmi/testcase1.java
+++ src/test/java/test/lakshmi/testcase1.java
@@ -25,6 +25,60 @@ public class testcase1 extends baseClass{
 		test.log(Status.INFO, "Test case Completed ");
 
 	}
+	
+	public static void createInstance(String fileName) {
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("./test-output/ExtentReports/"  + fileName);
+        htmlReporter.config().setTheme(Theme.STANDARD);
+        htmlReporter.config().setDocumentTitle("Marriott Automation Execution");
+        htmlReporter.config().setEncoding("utf-8");
+        htmlReporter.config().setReportName("Marriott Automation Execution");
+
+ 
+
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+    }
+    
+    
+    
+    @BeforeMethod
+    public static void hello() throws Exception {
+        createInstance("Reports.html");
+        
+        WebDriverManager.chromedriver().version("86").setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com");
+        driver.manage().window().maximize();
+        //getElement("CreateNewAccountButton");
+        //Desktop.getDesktop().browse(report);
+    }
+        
+    @Test()
+    public void launchExtent()
+    {
+        ExtentTest test = extent.createTest("sdfghjkl");
+        System.out.println("launched...");
+        
+    }
+    
+    
+    @AfterMethod
+    public void quit() throws MalformedURLException, URISyntaxException
+    {
+        
+    
+        driver.quit();
+        extent.flush();
+        
+        try {
+            String reportURL = new File("C:/Users/Riyazulla/eclipse-workspace/reader/test-output/ExtentReports").getAbsolutePath();
+            report = new URL("file:///" + reportURL + "/Reports.html").toURI();
+            Desktop.getDesktop().browse(report);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
 
 }
